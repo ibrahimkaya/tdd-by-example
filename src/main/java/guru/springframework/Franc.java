e1ff6008b8c04f457d0f177dd6212905f9393b6c
ibrahimkaya <kayaibrahim0@yandex.com>
2020-10-02 18:30:21 +0300

    Basic factory implementations and add currency

--- src/main/java/guru/springframework/Franc.java
+++ src/main/java/guru/springframework/Franc.java
@@ -5,11 +5,12 @@ package guru.springframework;
  * @create 2.10.2020 16:01
  */
 public class Franc extends Money {
-    public Franc(int amount) {
-        this.amount = amount;
+
+    public Franc(int amount, String currency) {
+        super(amount, currency);
     }
 
-    Franc times(int multiplier) {
-        return new Franc(amount * multiplier);
+    public Money times(int multiplier) {
+        return Money.franc(amount * multiplier);
     }
 }
--- src/main/java/guru/springframework/Money.java
+++ src/main/java/guru/springframework/Money.java
@@ -6,16 +6,37 @@ import java.util.Objects;
  * @Author ibrahim
  * @create 2.10.2020 16:25
  */
-public class Money {
+public abstract class Money {
 
     protected int amount;
+
+    protected String currency;
+
+    public Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
+
+    public abstract Money times(int multiplier);
+
+    public static Money dollar(int amount) {
+        return new Dollar(amount, "USD");
+    }
+
+    public static Money franc(int amount) {
+        return new Franc(amount, "CHF");
+    }
+
+    public String currency() {
+        return this.currency;
+    }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        Money Money = (Money) o;
-        return amount == Money.amount;
+        Money money = (Money) o;
+        return amount == money.amount;
     }
 
     @Override
